de359b950058f1732e4d47d763f3b4cd445cb374
yinm <yinm.cminor9@gmail.com>
2018-05-05 17:56:23 +0900

    Add directive to styles

--- src/components/atoms/Balloon/index.js
+++ src/components/atoms/Balloon/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import styles from './styles.css'
 
-const Balloon = ({ children }) => (
-  <span className={ styles.balloon }>{ children }</span>
+const Balloon = ({ children, className, ...props }) => (
+  <span className={ [ styles.balloon, className ].join(' ') } { ...props }>{children}</span>
 )
 
 export default Balloon
--- src/components/atoms/Balloon/index.stories.js
+++ src/components/atoms/Balloon/index.stories.js
@@ -4,3 +4,4 @@ import Balloon from './index.js'
 export default stories => stories
   .add('2文字ラベル', () => <Balloon>次へ</Balloon>)
   .add('4文字ラベル', () => <Balloon>削除する</Balloon>)
+  .add('左上から200px に配置', () => <Balloon style={{ position: 'absolute', top: '200px', left: '200px' }}>左上から200px に配置</Balloon>)
