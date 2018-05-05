6f76cc45db67f31a0b6d34976f2c7d3d5be8461f
yinm <yinm.cminor9@gmail.com>
2018-05-05 18:54:52 +0900

    Check container component and presentational component

--- src/components/atoms/Heading/index.js
+++ src/components/atoms/Heading/index.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import styles from './styles.css'
 
-const Heading = ({
+export const HeadingPresenter = ({
   children,
-  level = 2,
+  tag:Tag,
   visualLevel,
   className,
   ...props,
-}) => {
-  level = Math.max(0, Math.min(6, level))
-  visualLevel = (typeof visualLevel !== 'undefined') ? visualLevel : level
-  const Tag = `h${ level }`
-  const tagStyle = `${ styles.h } ${ styles[`h${ visualLevel }`] }`
+}) => (
+  <Tag className={[ styles.h, styles[`h${ visualLevel }`], className ].join(' ')} { ...props }>{children}</Tag>
+)
 
-  return (
-    <Tag className={ [ tagStyle, className ].join(' ') } { ...props }>
-      { children }
-    </Tag>
-  )
+export const HeadingContainer = ({
+  children,
+  presenter,
+  level = 2,
+  visualLevel,
+  ...props,
+}) => {
+  level = Math.max(1, Math.min(6, level))
+  visualLevel = (typeof visualLevel !== 'undefined') ? visualLevel : level
+  const tag = `h${ level }`
+
+  return presenter({ children, tag, visualLevel, props })
 }
+
+const Heading = props => (
+  <HeadingContainer presenter={ presenterProps => <HeadingPresenter { ...presenterProps } /> } { ...props } />
+)
 
 export default Heading
