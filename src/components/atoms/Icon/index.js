a8a01ae21f99ac57db361b547bd439c96bcba278
yinm <yinm.cminor9@gmail.com>
2018-05-06 15:02:56 +0900

    Add clicable styles

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import styles from './styles.css'
 
 export const TrashCanIcon = ({
   height = 20,
   width = 20,
+  className = '',
+  onClick,
   ...props,
-}) => (
-  <img
-    src="/icons/trash-can.svg"
-    alt=""
-    height={ height }
-    width={ width }
-    { ...props }
-  />
-)
+}) => {
+  if (onClick) className += `${ styles.clickable }`
+
+  return (
+    <img
+      src="/icons/trash-can.svg"
+      alt=""
+      height={ height }
+      width={ width }
+      className={ className }
+      onClick={ onClick }
+      { ...props }
+    />
+  )
+}
--- src/components/atoms/Icon/index.stories.js
+++ src/components/atoms/Icon/index.stories.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { action } from '@storybook/addon-actions'
 import { TrashCanIcon } from './index.js'
 
 export default stories => stories
   .add('TrashCanIcon', () => <TrashCanIcon />)
+  .add('Clicable', () => <TrashCanIcon onClick={ action('アイコンがクリックされました') }/>)
