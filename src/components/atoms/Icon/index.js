10c300e16a8a1749e9f41e88a6ea07f35e8a90eb
yinm <yinm.cminor9@gmail.com>
2018-05-06 14:58:07 +0900

    Add Icon atom

---
+++ src/components/atoms/Icon/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const TrashCanIcon = ({
+  height = 20,
+  width = 20,
+  ...props,
+}) => (
+  <img
+    src="/icons/trash-can.svg"
+    alt=""
+    height={ height }
+    width={ width }
+    { ...props }
+  />
+)
---
+++ src/components/atoms/Icon/index.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { TrashCanIcon } from './index.js'
+
+export default stories => stories
+  .add('TrashCanIcon', () => <TrashCanIcon />)
