6d26b8011fa7ff510b3bf67e82cd14735e62a4e8
yinm <yinm.cminor9@gmail.com>
2018-06-21 23:31:54 +0900

    Add Chevron

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -1,23 +1,54 @@
 import React from 'react'
 import styles from './styles.css'
 
-export const TrashCanIcon = ({
+export const TrashCanIconPresenter = ({
   height = 20,
   width = 20,
-  className = '',
+  ...props,
+}) => (
+  <img
+    src="/icons/trash-can.svg"
+    alt=""
+    height={ height }
+    width={ width }
+    { ...props }
+  />
+)
+
+export const IconContainer = ({
+  presenter,
   onClick,
+  className = '',
   ...props,
 }) => {
-  if (onclick) className += `${ styles.clickable }`
-  return (
-    <img
-      src="/icons/trash-can.svg"
-      alt=""
-      height={ height }
-      width={ width }
-      className={ className }
-      onClick={ onClick }
-      { ...props }
-    />
-  )
+  if (onClick) className += `${ styles.clickable }`
+  return presenter({ onclick, className, ...props })
 }
+
+export const TrashCanIcon = props => (
+  <IconContainer
+    presenter={ presenterProps => <TrashCanIconPresenter { ...presenterProps } /> }
+    { ...props }
+  />
+)
+
+export const ChevronRightIconPresenter = ({
+  height = 20,
+  width = 20,
+  ...props,
+}) => (
+  <img
+    src="/icons/chevron-right.svg"
+    alt=""
+    height={ height }
+    width={ width }
+    { ...props }
+  />
+)
+
+export const ChevronRightIcon = props => (
+  <IconContainer
+    presenter={ presenterProps => <ChevronRightIconPresenter { ...presenterProps } /> }
+    { ...props }
+  />
+)
--- src/components/atoms/Icon/index.stories.js
+++ src/components/atoms/Icon/index.stories.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { TrashCanIcon } from './index'
+import { TrashCanIcon, ChevronRightIcon } from './index'
 
 export default stories => stories
   .add('TrashCanIcon', () => <TrashCanIcon/>)
   .add('clickable', () => <TrashCanIcon onClick={ action('icon is clicked') }/>)
+  .add('ChevronRightIcon', () => <ChevronRightIcon/>)
