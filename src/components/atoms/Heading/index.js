2842f3de4945150d4bd9ffe2c4fdf26b1e12ecc1
yinm <yinm.cminor9@gmail.com>
2018-06-21 21:47:48 +0900

    Add level and visual level

--- src/components/atoms/Heading/index.js
+++ src/components/atoms/Heading/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import styles from './styles.css'
 
-export const Heading1 = ({ children, className, ...props }) => <h1 className={[ styles.h1, className ].join(' ') } { ...props }>{ children }</h1>
-export const Heading2 = ({ children, className, ...props }) => <h2 className={[ styles.h2, className ].join(' ') } { ...props }>{ children }</h2>
-export const Heading3 = ({ children, className, ...props }) => <h3 className={[ styles.h3, className ].join(' ') } { ...props }>{ children }</h3>
-export const Heading4 = ({ children, className, ...props }) => <h4 className={[ styles.h4, className ].join(' ') } { ...props }>{ children }</h4>
-export const Heading5 = ({ children, className, ...props }) => <h5 className={[ styles.h5, className ].join(' ') } { ...props }>{ children }</h5>
-export const Heading6 = ({ children, className, ...props }) => <h6 className={[ styles.h6, className ].join(' ') } { ...props }>{ children }</h6>
+const Heading = ({
+  children,
+  level = 2,
+  visualLevel,
+  className,
+  ...props,
+}) => {
+  level = Math.max(0, Math.min(6, level))
+  visualLevel = (typeof visualLevel !== 'undefined') ? visualLevel : level
+  const Tag = `h${ level }`
+  const tagStyle = `${ styles.h } ${ styles[`h${ visualLevel }`] }`
+
+  return (
+    <Tag className={ [ tagStyle, className ].join(' ') } { ...props }>{ children }</Tag>
+  )
+}
+
+export default Heading
