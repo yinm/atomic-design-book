c099796bb998703aa8b46d57751478d3e8de98c0
yinm <yinm.cminor9@gmail.com>
2018-06-21 21:15:50 +0900

    Add position

--- src/components/atoms/Balloon/index.stories.js
+++ src/components/atoms/Balloon/index.stories.js
@@ -4,3 +4,4 @@ import Balloon from './index'
 export default stories => stories
   .add('2文字ラベル', () => <Balloon>次へ</Balloon>)
   .add('4文字ラベル', () => <Balloon>削除する</Balloon>)
+  .add('absolute coordinate position', () => <Balloon style={{ position: 'absolute', top: '200px', left: '200px' }}>左上から200px</Balloon>)
