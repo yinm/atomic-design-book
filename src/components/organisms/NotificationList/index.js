ef60573dfbef255ff8dc864f301b39ac0bbdbd3c
yinm <yinm.cminor9@gmail.com>
2018-06-22 21:52:04 +0900

    Add NotificationList

---
+++ src/components/organisms/NotificationList/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './styles.css'
+import Notification from '../Notification/index'
+
+const NotificationList = ({
+  programs,
+  onClickDelete,
+  ...props,
+}) => (
+  <div { ...props }>
+    { programs.map((program, idx) => (
+      <Notification
+        key={ idx }
+        className={ styles.item }
+        program={ program }
+        onClickDelete={ onClickDelete }
+      />
+    )) }
+  </div>
+)
+
+export default NotificationList
