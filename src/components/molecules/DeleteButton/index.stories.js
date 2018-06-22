e1cd8379a8db49ed3db220eddfe4074f862d4ee5
yinm <yinm.cminor9@gmail.com>
2018-06-22 21:06:19 +0900

    Add decorator pattern

--- src/components/molecules/DeleteButton/index.stories.js
+++ src/components/molecules/DeleteButton/index.stories.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
 import DeleteButton from './index'
+import { withStyle } from '../../utils/decorators'
 
 export default stories => stories
-  .add('default', () => (
-    <DeleteButton onClick={ action('削除ボタンがクリックされました') } />
+  .add('default', () => withStyle({ margin: '50px' }) (
+    <DeleteButton onClick={ action('削除ボタンがクリクされました') } />
   ))
