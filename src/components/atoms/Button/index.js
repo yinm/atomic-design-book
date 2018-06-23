d6033cab469294ca4ca078029019f086968d2274
yinm <yinm.cminor9@gmail.com>
2018-06-23 15:19:52 +0900

    Add warning

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -9,5 +9,6 @@ function buttonFactory(type) {
 
 export const Button = buttonFactory('default');
 export const PrimaryButton = buttonFactory('primary');
+export const WarningButton = buttonFactory('warning');
 
 export default Button;
--- src/components/atoms/Button/index.stories.js
+++ src/components/atoms/Button/index.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Button, { PrimaryButton } from './index.js';
+import Button, { PrimaryButton, WarningButton } from './index.js';
 
 export default stories => stories
   .add('デフォルト', () => <Button>デフォルト</Button>)
-  .add('プライマリ', () => <PrimaryButton>プライマリ</PrimaryButton>);
+  .add('プライマリ', () => <PrimaryButton>プライマリ</PrimaryButton>)
+  .add('ウォーニング', () => <WarningButton>ウォーニング</WarningButton>)
