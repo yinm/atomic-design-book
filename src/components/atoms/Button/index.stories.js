6d1eb1ba80f8148427670295cdb4430a5e39ccf6
yinm <yinm.cminor9@gmail.com>
2018-06-23 15:44:37 +0900

    yarn checkpoint 4

--- src/components/atoms/Button/index.stories.js
+++ src/components/atoms/Button/index.stories.js
@@ -4,4 +4,4 @@ import Button, { PrimaryButton, WarningButton } from './index.js';
 export default stories => stories
   .add('デフォルト', () => <Button>デフォルト</Button>)
   .add('プライマリ', () => <PrimaryButton>プライマリ</PrimaryButton>)
-  .add('ウォーニング', () => <WarningButton>ウォーニング</WarningButton>)
+  .add('警告', () => <WarningButton>警告</WarningButton>);
