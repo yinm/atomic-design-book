32a470671eb3a1e52fc00709318fa657273f7454
yinm <yinm.cminor9@gmail.com>
2018-06-23 12:13:38 +0900

    Check composable

--- src/components/organisms/Notification/index.js
+++ src/components/organisms/Notification/index.js
@@ -5,6 +5,7 @@ import Heading from '../../atoms/Heading/index.js';
 import { InfoTxt } from '../../atoms/Txt/index.js';
 import Time from '../../atoms/Time/index.js';
 import DeleteButton from '../../molecules/DeleteButton/index.js';
+import MediaObjectLayout from '../../atoms/MediaObjectLayout/index'
 
 export class NotificationContainer extends Component {
   constructor() {
@@ -31,20 +32,16 @@ export const NotificationPresenter = ({
   onClickDelete,
   ...props,
 }) => (
-  <section className={ [ styles.root, className ].join(' ') } { ...props }>
-    <div>
-      <Img src={ program.thumbnail } className={ styles.media } width="128" height="72" />
-    </div>
-    <div className={ styles.body }>
-      <Heading level={ 3 } visualLevel={ 6 }>{ program.title }</Heading>
-      <InfoTxt size="s">{ program.channelName }</InfoTxt>
-      <InfoTxt size="s" className={ styles.time }>
-        <Time format="MM月DD日(ddd)HH:mm">{ program.startAt }</Time> ～
-        <Time format="HH:mm">{ program.endAt }</Time>
-      </InfoTxt>
-      <DeleteButton onClick={ onClickDelete } className={ styles.del } />
-    </div>
-  </section>
+  <MediaObjectLayout tag="section" className={ [ styles.root, className ].join(' ') } { ...props }>
+    <Img src={ program.thumbnail } className={ styles.media } width="128" height="72" />
+    <Heading level={ 3 } visualLevel={ 6 }>{ program.title }</Heading>
+    <InfoTxt size="s">{ program.channelName }</InfoTxt>
+    <InfoTxt size="s" className={ styles.time }>
+      <Time format="MM月DD日(ddd)HH:mm">{ program.startAt }</Time> ～
+      <Time format="HH:mm">{ program.endAt }</Time>
+    </InfoTxt>
+    <DeleteButton onClick={ onClickDelete } className={ styles.del } />
+  </MediaObjectLayout>
 );
 
 const Notification = props => (
