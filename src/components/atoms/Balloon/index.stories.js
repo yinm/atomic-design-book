072329ba2229b62a524e5695ead5d8ff74edd516
yinm <yinm.cminor9@gmail.com>
2018-06-25 21:55:23 +0900

    Add Notes

--- src/components/atoms/Balloon/index.stories.js
+++ src/components/atoms/Balloon/index.stories.js
@@ -12,11 +12,14 @@ import React from 'react';
 import Balloon, { BalloonTip } from './index.js';
 import { TrashCanIcon } from '../Icon/index.js';
 import { withStyle } from '../../utils/decorators.js';
+import { withNotes } from '@storybook/addon-notes'
+
+const note = withNotes('読みやすさを考慮すると 10 文字までが適当。11 文字以上を表示したい場合は別のデザインを検討すること。')
 
 export default stories => stories
   .add('2文字', () => <Balloon>次へ</Balloon>)
   .add('4文字', () => <Balloon>削除する</Balloon>)
-  .add('ten', () => <Balloon>削除したかったらする</Balloon>)
+  .add('10文字ラベル', note(() => <Balloon>削除したかったらする</Balloon>))
   .add('twenty', () => <Balloon>削除したかったらするけど、どうしたいかな</Balloon>)
   .add('thirty', () => <Balloon>削除したかったらするけど、どうしたいかな。嫌なら、やめようか</Balloon>)
   .add('thirty and linefeed', () => <Balloon>削除したかったらするけど、どうしたいかな。<br />嫌なら、やめようか</Balloon>)
