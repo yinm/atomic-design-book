fb3cc617243e408b9e715e50ed896b5ee0283ee3
yinm <yinm.cminor9@gmail.com>
2018-06-25 09:36:16 +0900

    Add stories

--- src/components/atoms/Balloon/index.stories.js
+++ src/components/atoms/Balloon/index.stories.js
@@ -14,11 +14,9 @@ import { TrashCanIcon } from '../Icon/index.js';
 import { withStyle } from '../../utils/decorators.js';
 
 export default stories => stories
-  .add('2文字ラベル', () => <Balloon>次へ</Balloon>)
-  .add('4文字ラベル', () => <Balloon>削除する</Balloon>)
-  .add('絶対座標配置', () => <Balloon style={{ position: 'absolute', top: '200px', left: '200px' }}>左上から 200px に配置</Balloon>)
-  .add('アイコンラベル', () => <Balloon><TrashCanIcon /></Balloon>)
-  .add('絵文字', () => <Balloon>❌</Balloon>)
-  .add('バルーンチップ', () => withStyle({ marginTop: '50px' })(
-    <p>ここに<BalloonTip label="注釈を記述するUI">バルーンチップ</BalloonTip>を表示</p>
-  ));
+  .add('2文字', () => <Balloon>次へ</Balloon>)
+  .add('4文字', () => <Balloon>削除する</Balloon>)
+  .add('ten', () => <Balloon>削除したかったらする</Balloon>)
+  .add('twenty', () => <Balloon>削除したかったらするけど、どうしたいかな</Balloon>)
+  .add('thirty', () => <Balloon>削除したかったらするけど、どうしたいかな。嫌なら、やめようか</Balloon>)
+  .add('thirty and linefeed', () => <Balloon>削除したかったらするけど、どうしたいかな。<br />嫌なら、やめようか</Balloon>)
