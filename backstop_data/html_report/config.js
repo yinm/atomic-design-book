70b7815ad9fefd419f8cf99abc9dacc5c25de502
yinm <yinm.cminor9@gmail.com>
2018-06-29 22:13:48 +0900

    Check liquid layout

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gihyo_ui_component_Balloon_4_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180629-214820/gihyo_ui_component_Balloon_4_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_Balloon_4_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "gihyo_ui_component_Balloon_4_0_document_0_tablet.png",
         "label": "Balloon 4文字ラベル",
@@ -24,7 +24,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gihyo_ui_component_Balloon_4_0_document_1_pc.png",
-        "test": "../bitmaps_test/20180629-214820/gihyo_ui_component_Balloon_4_0_document_1_pc.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_Balloon_4_0_document_1_pc.png",
         "selector": "document",
         "fileName": "gihyo_ui_component_Balloon_4_0_document_1_pc.png",
         "label": "Balloon 4文字ラベル",
@@ -44,7 +44,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gihyo_ui_component_Notification_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180629-214820/gihyo_ui_component_Notification_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_Notification_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "gihyo_ui_component_Notification_0_document_0_tablet.png",
         "label": "Notification",
@@ -64,7 +64,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gihyo_ui_component_Notification_0_document_1_pc.png",
-        "test": "../bitmaps_test/20180629-214820/gihyo_ui_component_Notification_0_document_1_pc.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_Notification_0_document_1_pc.png",
         "selector": "document",
         "fileName": "gihyo_ui_component_Notification_0_document_1_pc.png",
         "label": "Notification",
@@ -84,7 +84,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gihyo_ui_component_NotificationList_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180629-214820/gihyo_ui_component_NotificationList_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_NotificationList_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "gihyo_ui_component_NotificationList_0_document_0_tablet.png",
         "label": "NotificationList",
@@ -104,7 +104,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gihyo_ui_component_NotificationList_0_document_1_pc.png",
-        "test": "../bitmaps_test/20180629-214820/gihyo_ui_component_NotificationList_0_document_1_pc.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_NotificationList_0_document_1_pc.png",
         "selector": "document",
         "fileName": "gihyo_ui_component_NotificationList_0_document_1_pc.png",
         "label": "NotificationList",
@@ -124,7 +124,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gihyo_ui_component_NotificationList2Template_0_document_0_tablet.png",
-        "test": "../bitmaps_test/20180629-214820/gihyo_ui_component_NotificationList2Template_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_NotificationList2Template_0_document_0_tablet.png",
         "selector": "document",
         "fileName": "gihyo_ui_component_NotificationList2Template_0_document_0_tablet.png",
         "label": "NotificationList2Template",
@@ -144,12 +144,52 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/gihyo_ui_component_NotificationList2Template_0_document_1_pc.png",
-        "test": "../bitmaps_test/20180629-214820/gihyo_ui_component_NotificationList2Template_0_document_1_pc.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_NotificationList2Template_0_document_1_pc.png",
         "selector": "document",
         "fileName": "gihyo_ui_component_NotificationList2Template_0_document_1_pc.png",
         "label": "NotificationList2Template",
         "misMatchThreshold": 1e-10,
         "url": "http://localhost:9001/iframe.html?selectedKind=NotificationList2Template&selectedStory=default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/gihyo_ui_component_MailAuthForm_0_document_0_tablet.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_MailAuthForm_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "gihyo_ui_component_MailAuthForm_0_document_0_tablet.png",
+        "label": "MailAuthForm",
+        "misMatchThreshold": 1e-10,
+        "url": "http://localhost:9001/iframe.html?selectedKind=MailAuthForm&selectedStory=デフォルト",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/gihyo_ui_component_MailAuthForm_0_document_1_pc.png",
+        "test": "../bitmaps_test/20180629-221310/gihyo_ui_component_MailAuthForm_0_document_1_pc.png",
+        "selector": "document",
+        "fileName": "gihyo_ui_component_MailAuthForm_0_document_1_pc.png",
+        "label": "MailAuthForm",
+        "misMatchThreshold": 1e-10,
+        "url": "http://localhost:9001/iframe.html?selectedKind=MailAuthForm&selectedStory=デフォルト",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
